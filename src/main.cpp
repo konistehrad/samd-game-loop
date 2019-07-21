3e74128164a6867ae47ba225245c31adb14c5dfd
Conrad Kreyling <konistehrad@gmail.com>
2019-07-20 20:55:49 -0400

    Pre cat toy

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,10 @@
 #include <Arduino.h>
-#include <ArduinoLowPower.h>
 #include <Adafruit_NeoPixel.h>
 #include <Adafruit_SleepyDog.h>
 #include <limits.h>
-#include <IRLibGlobals.h>
-#include <IRLibRecvPCI.h>
-#include <IRLibDecodeBase.h>
-#include <IRLib_P02_Sony.h>
 #include <Thread.h>
+
+#include "FastDigital.h"
 
 #define NUMBER                float
 #define Time_t                unsigned long
@@ -27,38 +24,19 @@
 #define PIN_IR_PROX           (10u)
 #define PIN_IR_RECEIVER       (26u)
 
-inline void digitalWriteHigh(int PIN) {
-  PORT->Group[g_APinDescription[PIN].ulPort].OUTSET.reg = (1ul << g_APinDescription[PIN].ulPin);
-}
-
-inline void digitalWriteLow(int PIN) {
-  PORT->Group[g_APinDescription[PIN].ulPort].OUTCLR.reg = (1ul << g_APinDescription[PIN].ulPin);
-}
-
-inline int digitalReadDirect(int PIN) {
-  return !!(PORT->Group[g_APinDescription[PIN].ulPort].IN.reg & (1ul << g_APinDescription[PIN].ulPin));
-}
-
-inline void digitalWriteDirect(int PIN, bool val) {
-  if(val)  digitalWriteHigh(PIN);
-  else     digitalWriteLow(PIN);
-}
+#define DELTA(start,end) ((end >= start) ? (end - start) : (ULONG_MAX - start) + end)
 
 volatile uint16_t brightness = 1;
 uint16_t hueWrap = 0;
 Adafruit_NeoPixel strip(LED_COUNT, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
-IRrecvPCI irReceiver(PIN_IR_RECEIVER);
-IRdecodeSony irDecoder;
 
 inline void decreaseBrightness(uint8_t amount) {
   brightness = amount > brightness ? 0 : brightness - amount;
-  // strip.setBrightness(brightness);
 }
 
 inline void increaseBrightness(uint8_t amount) {
-    uint16_t result = amount + brightness;
-    brightness = result > 255 ? 255 : result;
-    // strip.setBrightness(brightness);
+  uint16_t result = amount + brightness;
+  brightness = result > 255 ? 255 : result;
 }
 
 void commonSetup() {
@@ -78,101 +56,48 @@ void commonSetup() {
   pinMode(A0, OUTPUT);
 
   strip.begin();
-  // strip.fill(0xff0000);
-  // strip.setBrightness(brightness);
   strip.show();
 
-  irReceiver.enableIRIn(); // Start the receiver
+  // irReceiver.enableIRIn(); // Start the receiver
 }
 
-void tryParseIR() {
-  if(irReceiver.getResults()) {
-    if(irDecoder.decode() && irDecoder.protocolNum == SONY) {
-      switch(irDecoder.value) {
-        case 0x240C: increaseBrightness(1); break; // vol up
-        case 0x640C: decreaseBrightness(1); break; // vol down
-        case 0x6F0D: hueWrap += 500; break; // arrow right
-        case 0x2F0D: hueWrap -= 500; break; // arrow left
-        case 0x0F0D: increaseBrightness(20); break; // arrow up
-        case 0x4F0D: decreaseBrightness(20); break; // arrow down
-        case 0x180C: break; // enter
-        default:     irDecoder.dumpResults(false);
-      }
-    }
-    irReceiver.enableIRIn();
-  }
-}
-
-bool frameDropped = false;
 bool state = false;
-unsigned long now = 0;
-unsigned long lastTime = 0;
-float dt;
-
-void ledBusyFlash() {
-  state = !state;
-  strip.setPixelColor(LED_COUNT - 1, state ? 20 : 0, 0, 0);
-}
+Time_t now = 0;
+Time_t lastTime = 0;
+Time_t dt;
 
 void animTick() {
-  hueWrap += ((USHRT_MAX / 4) * dt);
+  if(digitalReadDirect(PIN_SLIDE_SWITCH)) {
+    if(digitalReadDirect(PIN_BUTTON_LEFT)) {
+      hueWrap -= map(dt, 0, 4000, 0, USHRT_MAX);
+    } else if(digitalReadDirect(PIN_BUTTON_RIGHT)) {
+      hueWrap += map(dt, 0, 4000, 0, USHRT_MAX);
+    }
+  } else {
+    if(digitalReadDirect(PIN_BUTTON_LEFT)) {
+      decreaseBrightness( map(dt, 0, 1000, 0, 255) );
+    } else if(digitalReadDirect(PIN_BUTTON_RIGHT)) {
+      increaseBrightness( map(dt, 0, 1000, 0, 255) );
+    }
+  }
+
   auto color = strip.ColorHSV(hueWrap, 255, brightness);
   strip.setPixelColor(0, color);
   strip.show();
 }
 
-Thread ledFlashThread = Thread(ledBusyFlash, 200);
-Thread animationThread = Thread(animTick, MS_PER_FRAME);
-Thread irThread = Thread(tryParseIR, 0);
-
-void leftButtonInterrupt() {
-  if(digitalReadDirect(PIN_BUTTON_LEFT)) {
-    decreaseBrightness(10);
-  }
-}
-
-void rightButtonInterrupt() {
-  if(digitalReadDirect(PIN_BUTTON_RIGHT)) {
-    increaseBrightness(10);
-  }
-}
-
 void setup() {
   commonSetup();
-  LowPower.attachInterruptWakeup(PIN_BUTTON_LEFT, leftButtonInterrupt, CHANGE);
-  LowPower.attachInterruptWakeup(PIN_BUTTON_RIGHT, rightButtonInterrupt, CHANGE);
   lastTime = millis();
 }
 
-inline Time_t getDeltaTime(Time_t start, Time_t end) {
-  return (end >= start) ? 
-    (end - start) :            // standard delta
-    (ULONG_MAX - start) + end; // wraparound case
-}
-
 void loop() {
-  bool irReading = recvGlobal.currentState == STATE_RUNNING || recvGlobal.currentState == STATE_FINISHED;
   now = millis();
-  Time_t deltaMs = getDeltaTime(lastTime, now);
-  dt = deltaMs / 1000.0;
-
-  Time_t startUs = micros();
-  if(ledFlashThread.shouldRun(now)) ledFlashThread.run();
-  if(animationThread.shouldRun(now)) animationThread.run();
-  if(irThread.shouldRun(now)) irThread.run();
-  Time_t endUs = micros();
-
+  dt = DELTA(lastTime, now);
+  animTick();
   lastTime = now;
-  Time_t frameUs = getDeltaTime(startUs, endUs);
-  frameDropped = (frameUs > US_PER_FRAME);
-  auto frameMs = frameDropped ? MS_PER_FRAME : frameUs / 1000;
-  if(!irReading && frameMs < MS_PER_FRAME) {
-    auto sleepTime = MS_PER_FRAME - frameMs;
-    Watchdog.enable(sleepTime, true);
-    LowPower.idle();
-    Watchdog.disable();
-    
-    state = !state;
-    digitalWriteDirect(LED_BUILTIN, state);
-  } 
+
+  digitalWriteDirect(LED_BUILTIN, digitalReadDirect(PIN_SLIDE_SWITCH));
+  delay(MS_PER_FRAME / 2);
+  // Watchdog.sleep(MS_PER_FRAME / 2);
 }
